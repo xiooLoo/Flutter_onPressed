9e7f0f18c5e639a9dcce6b08e5eb0f6927176553
kbird <204138282@qq.com>
2018-08-21 09:00:45 +0800

    flutter_onPressed ,此项目为:[收藏/取消收藏☆], (First Commit)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Hello,flutter-2
+flutter_onPressed ,此项目为:[收藏/取消收藏☆]
--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,7 @@ class MyApp extends StatelessWidget {
                   child: Text(
                     'Oeschinen Lake Campground',
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
+                        fontWeight: FontWeight.bold, color: Colors.green),
                   ),
                 ),
                 Text(
@@ -31,35 +30,114 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          Icon(
+          /*Icon(
             Icons.star,
             color: Colors.red[500],
           ),
-          Text('41'),
+          Text('41'),*/
+          new FavouriteWidget(),
         ],
       ),
     );
-    return MaterialApp(
-        title: 'Flutter Demo2333',
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Top Lakes55555'),
+
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+      return new Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Icon(icon, color: color),
+          new Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(label,
+                style: TextStyle(
+                    fontSize: 12.0, fontWeight: FontWeight.w400, color: color)),
           ),
-          body: ListView(
-            children: [
-              // Image.asset(
-              //   'images/lake.jpg',
-              //   width: 600.0,
-              //   height: 240.0,
-              //   fit: BoxFit.cover,
-              // ),
-              Text(
-                '66666666'
-              ),
-              titleSection,
-            ],
-          ),
-        ),
+        ],
       );
     }
+
+    Widget buttonSection = new Container(
+      child:
+          new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        buildButtonColumn(Icons.call, 'CALL'),
+        buildButtonColumn(Icons.near_me, 'ROUTE'),
+        buildButtonColumn(Icons.share, 'SHARE'),
+      ]),
+    );
+
+    Widget textSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Text(
+        '''
+        Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter Demo2333',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Top Lakes55555'),
+        ),
+        body: ListView(
+          children: [
+            new Image.asset(
+              'images/lake.jpg',
+              width: 600.0,
+              height: 240.0,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
+
+class FavouriteWidget extends StatefulWidget {
+  @override
+  _FavouriteWidgetState createState() => new _FavouriteWidgetState();
+}
+
+class _FavouriteWidgetState extends State<FavouriteWidget> {
+  bool _isFavourited = true;
+  int _favouriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        new Container(
+            padding: new EdgeInsets.all(0.0),
+            child: new IconButton(
+              icon: _isFavourited
+                  ? new Icon(Icons.star)
+                  : new Icon(Icons.star_border),
+              color: Colors.red[500],
+              onPressed: _toggleFavourite,
+            )),
+        new Text('$_favouriteCount')
+      ],
+    );
+  }
+
+  void _toggleFavourite() {
+    setState(() {
+      if (_isFavourited) {
+        _favouriteCount -= 1;
+        _isFavourited = false;
+      } else {
+        _favouriteCount += 1;
+        _isFavourited = true;
+      }
+    });
+  }
 }
